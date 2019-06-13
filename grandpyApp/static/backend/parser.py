db987a1ece473288bf674125d589aa8274913383
SIMONIN Cyril <simonin.cyril91@gmail.com>
2019-06-13 17:55:38 +0200

    sauvegarde

---
+++ grandpyApp/static/backend/parser.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+"""Contains the parser class which allows to cut a sentence."""
+
+import unicodedata
+import config as const
+from fuzzywuzzy import fuzz
+
+
+class Parser:
+    """The objects in this class are used to cut and clean sentences."""
+
+    def __init__(self):
+        """Initialise the words to delete."""
+        self.stop_words = const.stop_words_genrique + const.stop_word_added
+        self.stop_words = self.normalizer(self.stop_words)
+
+    def normalizer(self, receive):
+        """Clean word so they follow the same structure."""
+        if isinstance(receive, list):
+            receive = " ".join(receive)
+        receive = unicodedata.normalize(
+            'NFKD', receive
+            ).encode(
+                'ASCII', 'ignore'
+                ).decode()
+        receive = receive.lower()
+        receive = ''.join(
+          [x if x.isalpha() or x.isnumeric() else " " for x in receive]
+          ).split()
+        clean_list = list()
+        for word in receive:
+            if word not in clean_list:
+                clean_list.append(word)
+        return clean_list
+
+    def percent_correspondence(self, word_user):
+        """Return percentage of correspondence with the stop words list."""
+        purcent_return = 0
+        for word in self.stop_words:
+            percent_match = fuzz.ratio(word, word_user)
+            if percent_match > purcent_return:
+                purcent_return = percent_match
+        return purcent_return
+
+    def parse_the_phrase(self, phrase):
+        """
+        1.
+
+        Allow to cut sentences and keep only
+        the words to use for searches.
+        """
+        phrase = self.normalizer(phrase)
+        phrase_finally = list()
+        for word_user in phrase:
+            percent_of_corresp = self.percent_correspondence(word_user)
+            if percent_of_corresp < 90 and word_user not in phrase_finally:
+                if len(word_user) >= 3 or (word_user.isnumeric()):
+                    phrase_finally.append(word_user)
+        phrase_finally = " ".join(phrase_finally)
+        return phrase_finally
